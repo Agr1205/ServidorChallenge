6f8c56b2e711fa6103b00bdff17a0c66d09a890c
Ana <anariverg@gmail.com>
2022-08-11 20:27:54 -0300

    cambio en la linea 8

---
+++ encrip.js
@@ -0,0 +1,25 @@
+
+var encriptar = document.getElementById(texto);
+
+function reemplazar() {
+    encriptar.replace("e","enter");
+
+    encriptar.replace("o","ober");
+
+    encriptar.replace("i","imes");
+
+    encriptar.replace("a", "ai");
+
+    encriptar.replace("u","ufat");
+
+
+
+
+
+    }
+
+
+
+
+
+
