dc5994d4b713b53ee9decaefd054ee1e2d41a1e1
Ana <anariverg@gmail.com>
2022-09-01 14:58:38 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.

---
+++ script.js
@@ -0,0 +1,60 @@
+
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
+
+
+var inputText = document.querySelector(".inputText");
+var inputmensaje = document.querySelector(".mensaje");
+
+var vocales = ["e","i","a","o","u"];
+var palabras = ["enter","imes","ai","ober","ufat"];
+
+
+function encriptar() {
+    
+    let textEncriptar = inputText.value.toLowerCase();
+    let resultado = "";
+
+    for(var i = 0; i <textEncriptar.length; i++) {
+      
+        if (vocales.indexOf(textEncriptar[i])!= -1) {
+            resultado += palabras[(vocales.indexOf(textEncriptar[i]))]
+        } else {
+            resultado += textEncriptar[i];
+        }
+        
+    }
+   
+    inputmensaje.value = resultado
+    inputText.value = "";
+    
+}
+
+function desencriptar() {
+    let resultado = inputText.value;
+
+    for(let i = 0; i <palabras.length; i++) {
+        resultado = resultado.replaceAll(palabras[i], vocales[i]);
+
+    }
+
+    
+    inputmensaje.value = resultado;
+    inputText.value="";
+}
+
+
+
+
+function copiar() {
+    
+    inputmensaje.select();
+    inputmensaje.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    inputmensaje.value = "";
+}
+
